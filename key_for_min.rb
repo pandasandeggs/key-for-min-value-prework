a025770d82e7bab2cd77df456bfc8e0eb06f5ac3
Chantal Wallace <theteatreehouse@gmail.com>
2018-07-14 13:01:10 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,9 +7,12 @@ def key_for_min_value(name_hash)
   name_hash.collect do |key,value|
     if name_hash = {} 
       nil 
-    elsif value < the_value
+    else
+    name_hash.each do |key,value|
+      if value < the_value
       the_key = key
       the_value = value
+      end 
     end
   end
   the_key
